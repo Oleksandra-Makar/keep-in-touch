b33711f13015c604e3d585725befb01ff2c63e0e
Олександра Макар <oleksandra_makar@epam.com>
2021-03-28 18:19:33 +0300

    final comit

---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,71 @@
+import React, { Fragment, useContext } from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import AuthContext from '../../context/auth/authContext';
+import ContactContext from '../../context/contact/contactContext';
+
+
+const Navbar = ( {title, icon} ) => {
+
+const authContext = useContext(AuthContext);
+const contactContext = useContext(ContactContext);
+
+const { isAuthenticated, logout, user } = authContext;
+const { clearContacts } = contactContext;
+
+const onLogout = () => {
+    logout();
+    clearContacts();
+}
+
+const authLinks = (
+    <Fragment>
+        <li>Hello <span className="badge">{user && user.name}</span></li>
+        <li>
+            <a onClick={onLogout} href="#!">
+                <i className="fas fa-sign-out-alt"></i> {' '}
+                <span className="hide-sm">Logout</span>
+            </a>
+        </li>
+    </Fragment>
+);
+
+const guestLinks = (
+    <Fragment>
+    <li>
+        <Link to='/register'>Register</Link>
+    </li>
+    <li>
+        <Link to='/login'>
+             
+            <span className="badge"> <i className="fas fa-sign-in-alt"/> {' '} Login</span>
+        </Link>
+    </li>
+    </Fragment>
+)
+
+    return (
+        <div className="navbar bg-primary">
+            <h1>
+                <i className={icon}/> {title}
+            </h1>
+            <ul>
+                {
+                    isAuthenticated ? authLinks : guestLinks
+                }
+            </ul>
+        </div>
+    );
+};
+
+Navbar.propTypes = {
+    title: PropTypes.string.isRequired,
+    icon: PropTypes.string
+};
+  
+Navbar.defaultProps = {
+    title: 'Keep In Touch',
+    icon: 'fas fa-fingerprint'
+};
+
+export default Navbar;
